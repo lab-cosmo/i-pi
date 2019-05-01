1942d49e45fa6dce80e2442470d63a216b537a44
Venkat Kapil <venkat.kapil@gmail.com>
2019-05-01 11:44:40 +0200

    cleaned up the PP script for SCP

--- tools/py/get_Ascp.py
+++ tools/py/get_Ascp.py
@@ -1,89 +1,158 @@
 #!/usr/bin/env python2
-import numpy as np
-import io
+
+import os
 import sys
+import argparse
+import numpy as np
 from ipi.inputs.simulation import InputSimulation
 from ipi.utils.io.inputs import io_xml
 
-#! TODO make this better - more professional, and better commented. use remd-sort as an example
-path2iipi="./input.xml"
+# Disable
+def blockPrint():
+    sys.stdout = open(os.devnull, 'w')
 
-ifile = open(path2iipi, "r")
-xmlrestart = io_xml.xml_parse_file(ifile)
-ifile.close()
+# Restore
+def enablePrint():
+    sys.stdout = sys.__stdout__
 
-#text_trap = io.StringIO()
-#sys.stdout = text_trap
-isimul = InputSimulation()
-isimul.parse(xmlrestart.fields[0][1])
-simul = isimul.fetch()
-#sys.stdout = sys.__stdout__
+def get_A(path2iipi):
+    """
+    Parses the i-PI input to read the relevant data files
+    and returns the Helmholtz free energy obtained within the
+    self consitent phonons (SCP) approximation.
+    """
 
-prefix =  simul.syslist[0].motion.prefix
-max_iter = simul.syslist[0].motion.max_iter
-asr = simul.syslist[0].motion.asr
-m3 = simul.syslist[0].beads.m3[-1]
-M = np.diag(m3)
-iM = np.diag(1.0 / m3)
-sqM = np.diag(np.sqrt(m3))
-isqM = np.diag(1.0 / np.sqrt(m3))
-kbT = float(simul.syslist[0].ensemble.temp)
-beta = 1.0 / kbT
-findex = max_iter - 1
-findex = 18
-widening = 1.10
+    blockPrint()
+
+    # Parses the i-PI input file.
+    ifile = open(path2iipi, "r")
+    xmlrestart = io_xml.xml_parse_file(ifile)
+    ifile.close()
+
+    # Initializes the simulation class.
+    isimul = InputSimulation()
+    isimul.parse(xmlrestart.fields[0][1])
+    simul = isimul.fetch()
+
+    enablePrint()
+
+    # Obtains various parameters and relevant filenames.
+    prefix = simul.syslist[0].motion.prefix
+    max_iter = simul.syslist[0].motion.max_iter
+    kbT = float(simul.syslist[0].ensemble.temp)
+    beta = 1.0 / kbT
+
+    # Checks for ASR type to skip
+    # over zero modes.
+    asr = simul.syslist[0].motion.asr
+    if asr == "crystal":
+        nz = 3
+    elif asr == "molecule":
+        nz = 6
+
+    for i in range(max_iter):
+
+        try:
+            # Imports the q, iD, x, f from the i^th  SCP iteration.
+            # "0" stands for the SCP trial Hamiltonian.
+            # iD -> the covariance matrix of the canonical density in position representation.
+            # q  -> the position at minimum potential energy.
+            # K  -> the force constant matrix.
+            # V  -> the minimum potential energy.
+            # hw -> the frequencies.
+            # vH -> the ensemble average of the potential w.r.t the canonical density.
+            # AH -> the free energy.
+            iD0 = np.loadtxt(prefix + ".iD." + str(i))
+            q0 = np.loadtxt(prefix + ".q." + str(i))
+            K0 = np.loadtxt(prefix + ".K." + str(i))
+            V0 = np.loadtxt(prefix + ".V0." + str(i))
+            hw0 = np.loadtxt(prefix + ".w." + str(i))[nz:]
+            betahw0 = beta * hw0
+            vH0 = np.sum(hw0 * np.cosh(betahw0 / 2.0) /
+                         np.sinh(betahw0 / 2.0) * 0.250)
+            AH0 = np.sum(hw0 * 0.5 + kbT * np.log(1 - np.exp(-betahw0)))
+        except IOError:
+            break
+
+        # Stores the potential /free energy of the initial harmonic trail Hamiltonian.
+        if i == 0:
+            A_harm = AH0
+            v_harm = vH0
+            V0_harm = V0
+            print "%23s %23s %23s %23s %23s %23s %23s" % (
+                "# ITERATION", "SCP FREE ENERGY", "SCP FREE ENERGY CORR",
+                "ERROR", "SCP INT ENERGY", "SCP INT ENERGY CORR ", "ERROR")
+
+        # Initializes the average and the error in the difference between the physical
+        # and the SCP potential.
+        avg_dv = 0.0
+        err_dv = 0.0
+        norm = 0
+
+        # Inner loop over previous SCP steps
+        for j in range(i + 1):
+
+            try:
+                # Imports the q, iD of the j^th trial Hamiltonian.
+                # The idea is to reweight using samples drawn from the j <= i trial Hamiltonians.
+                iD = np.loadtxt(prefix + ".iD." + str(j))
+                q = np.loadtxt(prefix + ".q." + str(j))
+
+                # x  -> the samples drawn from the canonical density of the trial distribution.
+                # v  -> the potential enegry of the samples.
+                x = np.loadtxt(prefix + ".x." + str(j))
+                v = np.loadtxt(prefix + ".v." + str(j))
+            except IOError:
+                break
+
+            # vh -> the harmonic component of the potential energy.
+            # Note that the harmonic Hamiltonian is the i^th one
+            # while the samples are drawn from the j^th one.
+            vh = 0.5 * np.sum(np.dot(x - q0, K0.T) * (x - q0), axis=1)
+
+            # Calculates the statistical weight of each sample.
+            w = np.exp(-(0.50 * np.dot(iD0, (x - q0).T).T * (x - q0)).sum(axis=1) +
+                       (0.50 * np.dot(iD, (x - q).T).T * (x - q)).sum(axis=1))
+            V1 = np.sum(w)
+            V2 = np.sum(w**2)
+
+            # Calculates the average amd error (over the samples from the j^th batch of samples)
+            # associated with the "anharmonic" component of the potential.
+            avg_dv_j = np.nan_to_num(np.sum(w * (v - vh)) / V1)
+            err_dv_j = np.nan_to_num(
+                np.sum(w * (v - vh - avg_dv_j)**2) / (V1 - V2 / V1))
+
+            # Calculates the "batch" weight.
+            c = np.nan_to_num(np.exp(-np.var(np.log(w))))
+
+            # Accumulates the contribution to the average (and error) from the j^th batch.
+            avg_dv += c * avg_dv_j
+            err_dv += c**2 * err_dv_j
+            norm += c
+
+        avg_dv = np.nan_to_num(avg_dv / norm)
+        err_dv = np.nan_to_num(err_dv / norm**2 / len(w))
+
+        # Calculates the SCP potential / free energy.
+        A_scp = AH0 + avg_dv
+        A_scp_err = np.sqrt(err_dv)
+        E_scp = 2.0 * vH0 + avg_dv
+        E_scp_err = np.sqrt(err_dv)
+        # Calculates the SCP potential / free energy correction
+        # w.r.t the initial trial Hamiltonian.
+        A_scp_corr = A_scp - A_harm - V0_harm
+        E_scp_corr = E_scp - 2.0 * v_harm - V0_harm
+
+        print "%23d %23.8e %23.8e %23.8e %23.8e %23.8e %23.8e" % (
+            i, A_scp, A_scp_corr, A_scp_err, E_scp, E_scp_corr, E_scp_err)
 
 
-if asr == "crystal":
-  nz = 3
-elif asr == "molecule":
-  nz = 6
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Calculates the Helmholtz free enegry within the self consistent phonons approximation.")
+    parser.add_argument("-i", "--input_xml", type=str, required=True, 
+                        help="Path to the i-PI xml file that was used to run the SCP calculation.")
+    args = parser.parse_args()
+    path2iipi = args.input_xml
 
-print "# %23s %23s %23s %23s %23s %23s %23s" % ("ITERATION", "SCP FREE ENERGY", "SCP FREE ENERGY CORR", "ERROR", "SCP ENERGY", "SCP INT ENERGY CORR ", "ERROR")
-for i in range(max_iter):
-    # Imports the q, iD, x, f from the i^th  SCP iteration.
-    iD0 = np.loadtxt(prefix + ".iD." + str(i))
-    q0 = np.loadtxt(prefix + ".q." + str(i))
-    K0 = np.loadtxt(prefix + ".K." + str(i))
-    V0 = np.loadtxt(prefix + ".V0." + str(i))
-    # Calculates the frequencies of the trial Hamiltonian.
-    hw0 = np.loadtxt(prefix + ".w." + str(i))[nz:]
-    betahw0 = beta * hw0
-    vH0 = V0 + np.sum(hw0 * np.cosh(betahw0 / 2.0) / np.sinh(betahw0 / 2.0) * 0.250)
-    AH0 = V0 + np.sum(hw0 * 0.5 + kbT * np.log(1 - np.exp(-betahw0)))
-    if i == 0:
-        Aharm = AH0
-        vharm = vH0
-        V0harm = V0
-    adv = 0.0
-    vdv = 0.0
-    norm = 0
-
-    # Inner loop over previous SCP steps
-    for j in range(i + 1):
-
-      # Imports the q, iD, x, f from the i^th  SCP iteration.
-      iD = np.loadtxt(prefix + ".iD." + str(j)) / widening**2
-      q = np.loadtxt(prefix + ".q." + str(j))
-      K = np.loadtxt(prefix + ".K." + str(j))
-      V = np.loadtxt(prefix + ".V0." + str(j))
-
-      x = np.loadtxt(prefix + ".x." + str(j))
-      v = np.loadtxt(prefix + ".v." + str(j))
-      vh = V0 + 0.5 * np.sum(np.dot(x - q0, K0.T) * (x - q0), axis=1)
-
-      # Calculates the weights to be used for reweighting.
-      w = np.exp(-(0.50 * np.dot(iD0, (x - q0).T).T * (x - q0)).sum(axis=1) + (0.50 * np.dot(iD, (x - q).T).T * (x - q)).sum(axis=1))
-      V1 = np.sum(w)
-      V2 = np.sum(w**2)
-      advj = np.nan_to_num(np.sum(w * (v - vh)) / V1)
-      vdvj = np.nan_to_num(sum(w * (v - vh - advj)**2) / (V1 - V2 / V1))
-      c = np.nan_to_num(1.0 / vdvj)
-      norm += c
-      adv += c * advj
-      vdv += c**2 * vdvj
-      #! TODO - think what to do with the error estimation, here it assumes uncorrelated random samples
-    adv = np.nan_to_num(adv / norm)
-    vdv = np.nan_to_num(vdv / norm**2 / len(w))
-
-    print "%23d %23.8e %23.8e %23.8e %23.8e %23.8e %23.8e" % (i, AH0 + adv, AH0 + adv - Aharm, np.sqrt(vdv), 2 * vH0 + vdv - V0, adv + 2.0 * vH0 - vharm * 2.0 + V0harm - V0, np.sqrt(vdv))
+    get_A(path2iipi)
