375e8214c09a87c28207e0f0ae4341ab87fd651c
Michele Ceriotti <michele.ceriotti@gmail.com>
2022-01-16 22:59:28 +0100

    Fixed a bug in harm3d - nonsensical virial

--- drivers/f90/driver.f90
+++ drivers/f90/driver.f90
@@ -75,7 +75,7 @@
 
       ! DMW
       DOUBLE PRECISION efield(3)
-      INTEGER i, j
+      INTEGER i, j, k
       
       ! parse the command line parameters
       ! intialize defaults
@@ -470,17 +470,17 @@
                virial = 0.0d0
                virial(1,1) = forces(1,1)*atoms(1,1)
             ELSEIF (vstyle == 30) THEN ! 3D harmonic potential
-                   pot = 0.0d0
-                   forces = 0.0d0
-                   virial = 0.0d0
-               DO i=1,nat 
-                   pot = pot + 0.5*ks*atoms(i,1)**2 + 0.5*ks*atoms(i,2)**2 + 0.5*ks*atoms(i,3)**2
-                   forces(i,1) = -ks*atoms(i,1)
-                   forces(i,2) = -ks*atoms(i,2)
-                   forces(i,3) = -ks*atoms(i,3)
-                   virial(i,1) = forces(i,1)*atoms(i,1)
-                   virial(i,2) = forces(i,2)*atoms(i,2)
-                   virial(i,3) = forces(i,3)*atoms(i,3)
+               pot = 0.0d0
+               forces = 0.0d0
+               virial = 0.0d0
+               DO i=1,nat
+                   forces(i, :) = -ks*atoms(i, :)                   
+                   do j=1,3
+                     do k=1,3
+                       virial(j,k) = virial(j,k) + forces(i,j)*atoms(i,k)
+                     enddo
+                   enddo 
+                   pot = 0.5d0*ks*(atoms(i,1)**2 + atoms(i,2)**2 + atoms(i,3)**2) + pot
                 enddo
             ELSEIF (vstyle == 7) THEN ! linear potential in x position of the 1st atom
                pot = ks*atoms(1,1)
